a335d0b4f06c21e00a12d6f14c1a6cf9ba5ed32c
quinnsells <quinnsells5@gmail.com>
2026-04-10 17:36:10 -0500

    initial commit of ast.h source file

---
+++ source/ast.h
@@ -0,0 +1,33 @@
+#ifndef AST_H
+#define AST_H
+
+#include <memory>
+#include "token.h"
+
+using namespace std;
+
+struct astNode {
+    virtual ~astNode() = default;
+};
+struct numberNode : astNode {
+    double value;
+
+    explicit numberNode(double value) : value(value) {}
+};
+struct unaryNode : astNode {
+    tokenType op;
+    unique_ptr<astNode> operand;
+
+    unaryNode(tokenType op, unique_ptr<astNode> operand) :
+            op(op), operand(move(operand)) {}
+};
+struct binaryNode : astNode {
+    tokenType op;
+    unique_ptr<astNode> left;
+    unique_ptr<astNode> right;
+
+    binaryNode(tokenType op, std::unique_ptr<astNode> left,
+               std::unique_ptr<astNode> right)
+        : op(op), left(std::move(left)), right(std::move(right)) {}
+};
+#endif
